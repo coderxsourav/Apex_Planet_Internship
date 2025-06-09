f6ce744c6e4e735c8df4a1fba7c6f76981280694
coderxsourav <codersourav2004@gmail.com>
2025-06-09 13:31:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+  const message = document.getElementById("message");
+  message.textContent = "Thanks for clicking!";
+}
+function showAlert() {
+    alert("You are Hacked!!!!");
+}
+
